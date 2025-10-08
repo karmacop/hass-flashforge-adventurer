a6d563179822c53788afdbc65a7f1cf16ccae29d
Glenn <glenn.tester@gmail.com>
2025-10-09 10:00:42 +1100

    Update protocol.py

--- custom_components/flashforge_adventurer_3/protocol.py
+++ custom_components/flashforge_adventurer_3/protocol.py
@@ -19,6 +19,7 @@ STATUS = '~M119'
 STATUS_REPLY_REGEX = re.compile('CMD M27 Received\.\r\nSD printing byte (\d+)\/(\d+)\r\nLayer: (\d+)\/(\d+)\r\n(.*?)ok\r\n')
 TEMPERATURE_REPLY_REGEX = re.compile('CMD M105 Received\.\r\nT0:(\d+\.?\d*)\/(\d+\.?\d*) T1:(\d+\.?\d*)\/(\d+\.?\d*) B:(\d+\.?\d*)\/(\d+\.?\d*)\r\nok\r\n')
 STATUS_REGEX = re.compile('CMD M119 Received.\r\nEndstop: X-max: \d+ Y-max: \d+ Z-min: \d+\r\nMachineStatus: ([\w\s]+)\r\nMoveMode: ([\w\s]+)\r\nStatus: S:\d+ L:\d+ J:\d+ F:\d+\r\nLED: \d+\r\nCurrentFile: (.*?)\r\nok\r\n')
+PRINTER_INFO_REGEX = re.compile(r'CMD M115 Received.\r\nMachine Type: (.*?)\r\nMachine Name: (.*?)\r\nFirmware: (.*?)\r\nSN: (.*?)\r\nX: (\d+) Y: (\d+) Z: (\d+)\r\nTool Count: (\d+)\r\nMac Address:(.*?)\n \r\nok\r\n')
 
 class PrinterStatus(TypedDict):
     online: bool
@@ -30,6 +31,15 @@ class PrinterStatus(TypedDict):
     desired_bed_temperature: Optional[float]
     nozzle_temperature: Optional[float]
     desired_nozzle_temperature: Optional[float]
+    type: Optional[str]
+    name: Optional[str]
+    fw: Optional[str]
+    sn: Optional[str]
+    max_x: Optional[int]
+    max_y: Optional[int]
+    max_z: Optional[int]
+    extruder_count: Optional[str]
+    mac: Optional[str]
 
 async def send_msg(reader: StreamReader, writer: StreamWriter, payload: str):
     msg = f'{payload}\r\n'
@@ -41,26 +51,28 @@ async def send_msg(reader: StreamReader, writer: StreamWriter, payload: str):
     return result.decode()
 
 
-async def collect_data(ip: str, port: int) -> Tuple[PrinterStatus, Optional[str], Optional[str]]:
+async def collect_data(ip: str, port: int) -> Tuple[PrinterStatus, Optional[str], Optional[str], Optional[str], Optional[str]]:
     future = asyncio.open_connection(ip, port)
     try:
         reader, writer = await asyncio.wait_for(future, timeout=TIMEOUT_SECONDS)
     except (asyncio.TimeoutError, OSError):
-        return { 'online': False }, None, None, None
+        return { 'online': False }, None, None, None, None
     response: PrinterStatus = { 'online': True }
     await send_msg(reader, writer, STATUS_COMMAND)
     print_job_info = await send_msg(reader, writer, PRINT_JOB_INFO_COMMAND)
     temperature_info = await send_msg(reader, writer, TEMPERATURE_COMMAND)
     status_info = await send_msg(reader, writer, STATUS)
+    printer_info = await send_msg(reader, writer, PRINTER_INFO_COMMAND)
     writer.close()
     await writer.wait_closed()
-    return response, print_job_info, temperature_info, status_info
+    return response, print_job_info, temperature_info, status_info, printer_info
 
 
-def parse_data(response: PrinterStatus, print_job_info: str, temperature_info: str, status_info:str) -> PrinterStatus:
+def parse_data(response: PrinterStatus, print_job_info: str, temperature_info: str, status_info:str, printer_info:str) -> PrinterStatus:
     print_job_info_match = STATUS_REPLY_REGEX.match(print_job_info)
     if print_job_info_match:
         response['progress'] = int(print_job_info_match.group(1))
+    
     temperature_match = TEMPERATURE_REPLY_REGEX.match(temperature_info)
     if temperature_match:
         # Printer is printing if desired temperatures are greater than zero. If not, it's paused.
@@ -71,19 +83,33 @@ def parse_data(response: PrinterStatus, print_job_info: str, temperature_info: s
         response['desired_nozzle_temperature'] = desired_nozzle_temperature
         response['bed_temperature'] = float(temperature_match.group(3))
         response['desired_bed_temperature'] = desired_bed_temperature
+    
     status_match = STATUS_REGEX.match(status_info)
     if status_match:
         response['machine_status'] = status_match.group(1)
         response['move_mode'] = status_match.group(2)
         response['current_file'] = status_match.group(3)
+    
+    printer_info_match = PRINTER_INFO_REGEX.match(printer_info)
+    if printer_info_match:
+        response['type'] = str(printer_info_match.group(1))
+        response['name'] = str(printer_info_match.group(2))
+        response['fw'] = str(printer_info_match.group(3))
+        response['sn'] = str(printer_info_match.group(4))
+        response['max_x'] = int(printer_info_match.group(5))
+        response['max_y'] = int(printer_info_match.group(6))
+        response['max_z'] = int(printer_info_match.group(7))
+        response['extruder_count'] = str(printer_info_match.group(8))
+        response['mac'] = str(printer_info_match.group(9))
+    
     return response
 
 
 async def get_print_job_status(ip: str, port: int) -> PrinterStatus:
-    response, print_job_info, temperature_info, status_info = await collect_data(ip, port)
+    response, print_job_info, temperature_info, status_info, printer_info = await collect_data(ip, port)
     if not response['online']:
         return response
-    return parse_data(response, print_job_info, temperature_info,status_info)
+    return parse_data(response, print_job_info, temperature_info, status_info, printer_info )
 
 
 if __name__ == '__main__':
